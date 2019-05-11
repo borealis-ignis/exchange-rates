5194cb5776a332afe4460b20fa086ab3034c182e
Kastalski Sergey <kasergey87@gmail.com>
2019-05-11 18:42:21 +0300

    GUI (chart drawing).

--- exchange-rates-application/erates-web/src/main/java/com/borealis/erates/controller/ERatesRestController.java
+++ exchange-rates-application/erates-web/src/main/java/com/borealis/erates/controller/ERatesRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +18,7 @@ import com.borealis.erates.service.ExchangeRatesService;
 /**
  * @author Kastalski Sergey
  */
-@RestController("/exchangerates")
+@RestController
 public class ERatesRestController {
 	
 	private static Logger logger = LoggerFactory.getLogger(ERatesRestController.class);
@@ -27,10 +30,16 @@ public class ERatesRestController {
 		this.eratesService = eratesService;
 	}
 	
-	@GetMapping("/")
-	public List<ExchangeRateDto> getExchangeRates(@RequestParam(required = false, name = "currencyId") final Long currencyId) {
+	@GetMapping(path = "/exchangerates")
+	public List<ExchangeRateDto> getExchangeRates(@RequestParam(required = true, name = "currencyId") final Long currencyId) {
 		final LocalDateTime defaultFromDate = LocalDateTime.now().minusMonths(1l);
 		return eratesService.getExchangeRates(defaultFromDate, currencyId);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handleError(final Exception ex) {
+		logger.error("Internal server error", ex);
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 }
--- exchange-rates-application/erates-web/src/main/resources/static/js/erates.js
+++ exchange-rates-application/erates-web/src/main/resources/static/js/erates.js
@@ -17,9 +17,123 @@ var clickCurrencyEvent = function() {
 	$currencyButton.addClass(activeClass);
 	
 	updateChartArea($currencyButton.attr("id"));
-}
+};
 
 function updateChartArea(currencyId) {
-	//TODO: send request to get rates (use flot to display it)
-	console.log("currencyId: " + currencyId);
+	var ctx = $("input#ctx").val();
+	
+	$.ajax({
+		dataType: "json",
+		url: ctx + "exchangerates?currencyId=" + currencyId,
+		success: chartUpdateEvent
+	});
+}
+
+var chartUpdateEvent = function(data) {
+	var buyRates = [];
+	var sellRates = [];
+	
+	for (var i in data) {
+		var item = data[i];
+		
+		var dateTime = new Date(item.updateDate).getTime();
+		
+		buyRates.push([dateTime, item.buyRate]);
+		sellRates.push([dateTime, item.sellRate]);
+	}
+	
+	
+	var dataset = [
+		{ label: "Sell Rate", data: sellRates },
+		{ label: "Buy Rate", data: buyRates }
+	];
+	
+	var options = {
+		series: {
+			lines: {
+				show: true
+			},
+			points: {
+				radius: 5,
+				fill: true,
+				show: true
+			}
+		},
+		xaxis: {
+			mode: "time",
+			//tickSize: [1, "day"],
+			timezone: "browser",
+			axisLabelUseCanvas: true,
+			axisLabelFontSizePixels: 12,
+			axisLabelFontFamily: 'Verdana, Arial',
+			axisLabelPadding: 10
+		},
+		yaxes: {
+			axisLabelUseCanvas: true,
+			axisLabelFontSizePixels: 12,
+			axisLabelFontFamily: 'Verdana, Arial',
+			axisLabelPadding: 10
+		},
+		legend: {
+			container:$("#legendContainer"),
+			noColumns: 0,
+		},
+		grid: {
+			hoverable: true,
+			clickable: true,
+			borderWidth: 1,
+			borderColor: "#d5fec6",
+			backgroundColor: { colors: ["#ffffff", "#d5fec6"] }
+		},
+		colors: ["#1fa403", "#0662c6"]
+	};
+	
+	var chartContainer = "div#charts_container div#chart";
+	$.plot(chartContainer, dataset, options);
+	$(chartContainer).UseTooltip();
+	$(chartContainer).UseClickTooltip();
+};
+
+
+// Plugins:
+var previousPoint = null;
+$.fn.UseTooltip = function () {
+	$(this).bind("plothover", toolTipEvent);
+};
+
+$.fn.UseClickTooltip = function () {
+	$(this).bind("plotclick", toolTipEvent);
+};
+
+var toolTipEvent = function (event, pos, item) {
+	if (item) {
+		if (previousPoint != item.dataIndex) {
+			previousPoint = item.dataIndex;
+			
+			$("#tooltip").remove();
+			
+			var date = item.datapoint[0];
+			var rate = item.datapoint[1];
+			showTooltip(item.pageX, item.pageY, "<strong>" + rate + "</strong><br/> " + dateFormat(new Date(date)));
+		}
+	} else {
+		$("#tooltip").remove();
+		previousPoint = null;
+	}
+};
+
+function dateFormat(date) {
+	var offsetSign = (date.getTimezoneOffset() < 0)? "-" : "+";
+	var offset = Math.abs(date.getTimezoneOffset() / 60);
+	
+	return date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2) + " "
+    	+ ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2)
+    	+ offsetSign + ("0" + offset).slice(-2) + "00";
+}
+
+function showTooltip(x, y, contents) {
+	$('<div id="tooltip">' + contents + '</div>').css({
+		top: y + 5,
+		left: x + 20,
+	}).appendTo("body").fadeIn(200);
 }
